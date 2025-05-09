ef16f218c16bfa9ef25600e2657b6edb12d1b7b7
ab-sid <absid24@gmail.com>
2025-05-09 14:32:33 +0600

    4 task done

--- index.ts
+++ index.ts
@@ -17,6 +17,31 @@ function formatString(input:string, toUpper?: boolean):string{
     return arrays.reduce((a,b)=>a.concat(b))
  }
 
+ class Vehicle{
+    private make:string;
+    private year:number;
 
+    constructor(make:string,year:number){
+        this.make = make;
+        this.year = year;
+    }
+    getInfo(){
+        console.log(`Make: ${this.make}, Year: ${this.year}`);
+    }
+ }
+
+ class Car extends Vehicle{
+    private model:string
+     constructor(make:string,year:number,model:string){
+        super(make,year)
+        this.model = model
+     }
+
+     getModel(){
+        console.log(`Model: ${this.model}`);
+     }
+ }
+
+ 
 
  
